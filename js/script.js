490e1d49a8b6d8ddffe080ecbc52d859c99cb56c
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-10-10 17:25:42 -0300

    Fazendo de um metodo mais "inteligente"

--- js/script.js
+++ js/script.js
@@ -23,294 +23,65 @@ $(function(){
   }
   )  
 },
-    $("#img_like").click(function(){
-    let like = $("#img_like")
-    let like_data = $("#like").data("like")
-    let dislike_data = $("#dislike").data("dislike")
+    $(".btn_like").click(function(){
+    let nome_btn = $("#" + this.id);
 
-    $("#dislike").text(dislike_data)
+    let nome_datalike = $("#" + $(this).closest('content').find('span')[0].id);
+    let like_data = $(nome_datalike).data("like");
+
+    let dislike = $("#" + $(this).closest('content').find('i')[1].id)
+    let nome_datadislike = "#" + $(this).closest('content').find('span')[1].id
+    let dislike_data = $(nome_datadislike).data("dislike");
+
+    $(nome_datadislike).text(dislike_data)
 
 
-    let dislike = $("#img_dislike")
-    if(like.hasClass("far fa-thumbs-up")){
-    $("#like").text(like_data+1)
-    like.removeClass("far fa-thumbs-up")
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-    like.addClass("fas fa-thumbs-up")
+    if(nome_btn.hasClass("far")){
+    $(nome_datalike).text(like_data+1)
+    nome_btn.removeClass("far")
+    dislike.removeClass("fas")
+    dislike.addClass("far")
+    nome_btn.addClass("fas")
     }
     else{
-    $("#like").text(like_data)
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
+    $(nome_datalike).text(like_data)
+    nome_btn.removeClass("fas")
+    nome_btn.addClass("far")
 
     }
 
 
 
-    }
-    ),
-    $("#img_dislike").click(function(){
-    let like = $("#img_like")
-    let like_data = $("#like").data("like")
-    let dislike_data = $("#dislike").data("dislike")
-
-    $("#like").text(like_data)
-    $("#dislike").text(dislike_data+1)
-    let dislike = $("#img_dislike")
-
-
-    if(dislike.hasClass("far fa-thumbs-down")){
-    $("#dislike").text(dislike_data+1)
-    dislike.removeClass("far fa-thumbs-down")
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-    dislike.addClass("fas fa-thumbs-down")
-    }
-    else{
-    $("#dislike").text(dislike_data)
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-
-    }
     }),
-    $("#img_like2").click(function(){
-    let like = $("#img_like2")
-    let like_data = $("#like2").data("like")
-    let dislike_data = $("#dislike2").data("dislike")
+    $(".btn_dislike").click(function(){
+        let nome_btn = $("#" + this.id);
+    
+        let nome_datalike = "#" + $(this).closest('div').find('span')[0].id;
+    
+        let like_data = $(nome_datalike).data("like");
+        let nome_datadislike = "#" + $(this).closest('content').find('span')[1].id
+        let dislike_data = $(nome_datadislike).data("dislike");
+    
+        $(nome_datalike).text(like_data)
+    
+    
+        let like = $("#" + $(this).closest('content').find('i')[0].id)
 
-    $("#dislike2").text(dislike_data)
-
-
-    let dislike = $("#img_dislike2")
-    if(like.hasClass("far fa-thumbs-up")){
-    $("#like2").text(like_data+1)
-    like.removeClass("far fa-thumbs-up")
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-    like.addClass("fas fa-thumbs-up")
-    }
-    else{
-    $("#like2").text(like_data)
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-
-    }
-    }),
-    $("#img_dislike2").click(function(){
-    let like = $("#img_like2")
-    let like_data = $("#like2").data("like")
-    let dislike_data = $("#dislike2").data("dislike")
-
-    $("#like2").text(like_data)
-    $("#dislike2").text(dislike_data+1)
-    let dislike = $("#img_dislike2")
-
-
-    if(dislike.hasClass("far fa-thumbs-down")){
-    $("#dislike2").text(dislike_data+1)
-    dislike.removeClass("far fa-thumbs-down")
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-    dislike.addClass("fas fa-thumbs-down")
-    }
-    else{
-    $("#dislike2").text(dislike_data)
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-
-    }
-    }),
-
-    $("#img_like3").click(function(){
-    let like = $("#img_like3")
-    let like_data = $("#like3").data("like")
-    let dislike_data = $("#dislike3").data("dislike")
-
-    $("#dislike3").text(dislike_data)
-
-
-    let dislike = $("#img_dislike3")
-    if(like.hasClass("far fa-thumbs-up")){
-    $("#like3").text(like_data+1)
-    like.removeClass("far fa-thumbs-up")
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-    like.addClass("fas fa-thumbs-up")
-    }
-    else{
-    $("#like3").text(like_data)
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-
-    }
-    }),
-    $("#img_dislike3").click(function(){
-    let like = $("#img_like3")
-    let like_data = $("#like3").data("like")
-    let dislike_data = $("#dislike3").data("dislike")
-
-    $("#like3").text(like_data)
-    $("#dislike3").text(dislike_data+1)
-    let dislike = $("#img_dislike3")
-
-
-    if(dislike.hasClass("far fa-thumbs-down")){
-    $("#dislike3").text(dislike_data+1)
-    dislike.removeClass("far fa-thumbs-down")
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-    dislike.addClass("fas fa-thumbs-down")
-    }
-    else{
-    $("#dislike3").text(dislike_data)
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-
-    }
-    }),
-
-    $("#img_like4").click(function(){
-    let like = $("#img_like4")
-    let like_data = $("#like4").data("like")
-    let dislike_data = $("#dislike4").data("dislike")
-
-    $("#dislike4").text(dislike_data)
-
-
-    let dislike = $("#img_dislike4")
-    if(like.hasClass("far fa-thumbs-up")){
-    $("#like4").text(like_data+1)
-    like.removeClass("far fa-thumbs-up")
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-    like.addClass("fas fa-thumbs-up")
-    }
-    else{
-    $("#like4").text(like_data)
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-
-    }
-    }),
-    $("#img_dislike4").click(function(){
-    let like = $("#img_like4")
-    let like_data = $("#like4").data("like")
-    let dislike_data = $("#dislike4").data("dislike")
-
-    $("#like4").text(like_data)
-    $("#dislike4").text(dislike_data+1)
-    let dislike = $("#img_dislike4")
-
-
-    if(dislike.hasClass("far fa-thumbs-down")){
-    $("#dislike4").text(dislike_data+1)
-    dislike.removeClass("far fa-thumbs-down")
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-    dislike.addClass("fas fa-thumbs-down")
-    }
-    else{
-    $("#dislike4").text(dislike_data)
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-
-    }
-    }),
-    $("#img_like5").click(function(){
-    let like = $("#img_like5")
-    let like_data = $("#like5").data("like")
-    let dislike_data = $("#dislike5").data("dislike")
-
-    $("#dislike5").text(dislike_data)
-
-
-    let dislike = $("#img_dislike5")
-    if(like.hasClass("far fa-thumbs-up")){
-    $("#like5").text(like_data+1)
-    like.removeClass("far fa-thumbs-up")
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-    like.addClass("fas fa-thumbs-up")
-    }
-    else{
-    $("#like5").text(like_data)
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-
-    }
-    }),
-    $("#img_dislike5").click(function(){
-    let like = $("#img_like5")
-    let like_data = $("#like5").data("like")
-    let dislike_data = $("#dislike5").data("dislike")
-
-    $("#like5").text(like_data)
-    $("#dislike5").text(dislike_data+1)
-    let dislike = $("#img_dislike5")
-
-
-    if(dislike.hasClass("far fa-thumbs-down")){
-    $("#dislike5").text(dislike_data+1)
-    dislike.removeClass("far fa-thumbs-down")
-    like.removeClass("fas fa-thumbs-up")
-    like.addClass("far fa-thumbs-up")
-    dislike.addClass("fas fa-thumbs-down")
-    }
-    else{
-    $("#dislike5").text(dislike_data)
-    dislike.removeClass("fas fa-thumbs-down")
-    dislike.addClass("far fa-thumbs-down")
-
-    }
-    }),
-    $("#img_like6").click(function(){
-    let like = $("#img_like6")
-    let like_data = $("#like6").data("like")
-    let dislike_data = $("#dislike6").data("dislike")
-
-    $("#dislike6").text(dislike_data)
-
-
-    let dislike = $("#img_dislike6")
-    if(like.hasClass("far fa-thumbs-up")){
-        $("#like6").text(like_data+1)
-        like.removeClass("far fa-thumbs-up")
-        dislike.removeClass("fas fa-thumbs-down")
-        dislike.addClass("far fa-thumbs-down")
-        like.addClass("fas fa-thumbs-up")
-    }
-    else{
-        $("#like6").text(like_data)
-        like.removeClass("fas fa-thumbs-up")
-        like.addClass("far fa-thumbs-up")
-
-    }
-    }),
-    $("#img_dislike6").click(function(){
-        let like = $("#img_like6")
-        let like_data = $("#like6").data("like")
-        let dislike_data = $("#dislike6").data("dislike")
-
-        $("#like6").text(like_data)
-        $("#dislike6").text(dislike_data+1)
-        let dislike = $("#img_dislike6")
-
-
-        if(dislike.hasClass("far fa-thumbs-down")){
-            $("#dislike6").text(dislike_data+1)
-            dislike.removeClass("far fa-thumbs-down")
-            like.removeClass("fas fa-thumbs-up")
-            like.addClass("far fa-thumbs-up")
-            dislike.addClass("fas fa-thumbs-down")
+        
+        if(nome_btn.hasClass("far")){
+        $(nome_datadislike).text(dislike_data+1)
+        nome_btn.removeClass("far")
+        like.removeClass("fas")
+        like.addClass("far")
+        nome_btn.addClass("fas")
         }
         else{
-            $("#dislike6").text(dislike_data)
-            dislike.removeClass("fas fa-thumbs-down")
-            dislike.addClass("far fa-thumbs-down")
-
+        $(nome_datadislike).text(dislike_data)
+        nome_btn.removeClass("fas")
+        nome_btn.addClass("far")
+    
         }
-        }),
+    }),
 )
 
 //Aqui eu valido o formulario da pagina de contatos
